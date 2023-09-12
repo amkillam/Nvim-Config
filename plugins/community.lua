b8236e56a91cb4f3ebc8eb8c68ef840a8eb5ae62
amkillam <amkillam@mun.ca>
2023-09-12 14:52:24 -0230

    More community changes

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,8 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack" },
+  { import = "astrocommunity.pack.typescript-all-in-one", enabled = false },
+  { import = "astrocommunity.pack.typescript-deno", enabled = false },
   { import = "astrocommunity.colorscheme" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
