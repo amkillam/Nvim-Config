4f6a7e9811635fea53df2456bba6e1d517cd1b4d
Adam Killam <adammkillam@gmail.com>
2024-11-17 09:46:11 -0330

    Fix nvim-ufo on Mac

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -5,68 +5,128 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      -- Configure core features of AstroNvim
+      features = {
+        large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+        autopairs = true, -- enable autopairs at start
+        cmp = true, -- enable completion at start
+        diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+        highlighturl = true, -- highlight URLs at start
+        notifications = true, -- enable notifications at start
       },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
+      -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+      diagnostics = {
+        virtual_text = true,
+        underline = true,
       },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs with `H` and `L`
-        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bD"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Pick to close",
+      -- vim options can be configured here
+      options = {
+        opt = { -- vim.opt.<key>
+          relativenumber = true, -- sets vim.opt.relativenumber
+          number = true, -- sets vim.opt.number
+          spell = false, -- sets vim.opt.spell
+          signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+          wrap = false, -- sets vim.opt.wrap
         },
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        ["<Leader>b"] = { desc = "Buffers" },
-        -- quick save
-        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        g = { -- vim.g.<key>
+          -- configure global vim variables (vim.g)
+          -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+          -- This can be found in the `lua/lazy_setup.lua` file
+        },
       },
-      t = {
-        -- setting a mapping to false will disable it
-        -- ["<esc>"] = false,
+      -- Mappings can be configured through AstroCore as well.
+      -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- second key is the lefthand side of the map
+
+          -- navigate buffer tabs with `H` and `L`
+          L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+          H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
+          -- mappings seen under group name "Buffer"
+          ["<Leader>bD"] = {
+            function()
+              require("astroui.status.heirline").buffer_picker(
+                function(bufnr) require("astrocore.buffer").close(bufnr) end
+              )
+            end,
+            desc = "Pick to close",
+          },
+          -- tables with just a `desc` key will be registered with which-key if it's installed
+          -- this is useful for naming menus
+          ["<Leader>b"] = { desc = "Buffers" },
+          -- quick save
+          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
       },
+    },
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    version = false,
+    event = { "User AstroFile", "InsertEnter" },
+    specs = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          maps.n["zR"] = { function() require("ufo").openAllFolds() end, desc = "Open all folds" }
+          maps.n["zM"] = { function() require("ufo").closeAllFolds() end, desc = "Close all folds" }
+          maps.n["zr"] = { function() require("ufo").openFoldsExceptKinds() end, desc = "Fold less" }
+          maps.n["zm"] = { function() require("ufo").closeFoldsWith() end, desc = "Fold more" }
+          maps.n["zp"] = { function() require("ufo").peekFoldedLinesUnderCursor() end, desc = "Peek fold" }
+        end,
+      },
+      {
+        "AstroNvim/astrolsp",
+        optional = true,
+        opts = function(_, opts)
+          local astrocore = require "astrocore"
+          if astrocore.is_available "nvim-ufo" then
+            opts.capabilities = astrocore.extend_tbl(opts.capabilities, {
+              textDocument = { foldingRange = { dynamicRegistration = false, lineFoldingOnly = true } },
+            })
+          end
+        end,
+      },
+    },
+    dependencies = { { "kevinhwang91/promise-async", lazy = true } },
+    opts = {
+      preview = {
+        mappings = {
+          scrollB = "<C-B>",
+          scrollF = "<C-F>",
+          scrollU = "<C-U>",
+          scrollD = "<C-D>",
+        },
+      },
+      provider_selector = function(_, filetype, buftype)
+        local function handleFallbackException(bufnr, err, providerName)
+          if type(err) == "string" and err:match "UfoFallbackException" then
+            return require("ufo").getFolds(bufnr, providerName)
+          else
+            return require("promise").reject(err)
+          end
+        end
+
+        return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+          or function(bufnr)
+            return require("ufo")
+              .getFolds(bufnr, "lsp")
+              :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
+              :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
+          end
+      end,
     },
   },
 }
