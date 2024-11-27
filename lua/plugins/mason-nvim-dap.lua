606af731bc89102abf7610d8c28fae117603bd53
Adam Killam <adammkillam@gmail.com>
2024-11-27 11:52:05 -0330

    Add more plugins

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -1,11 +1,6 @@
 return {
   "jay-babu/mason-nvim-dap.nvim",
-  -- overrides `require("mason-nvim-dap").setup(...)`
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "codelldb",
-      "cpptools",
-    })
-  end,
+  opts = {
+    ensure_installed = { "python", "rust", "cpp", "c", "csharp" },
+  },
 }
